e44989c070eed46ce11aa03b658d8fcdd7eb7d8d
Andrey Strelnikov <xoz9ih@outlook.com>
2024-11-01 17:20:58 +0300

    сделал основной функционал по заданию

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,20 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from '../slices/root/rootReducer';
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from '../slices/root/rootReducer'
 import {
-  TypedUseSelectorHook,
-  useDispatch as dispatchHook,
-  useSelector as selectorHook
-} from 'react-redux';
+	TypedUseSelectorHook,
+	useDispatch as dispatchHook,
+	useSelector as selectorHook,
+} from 'react-redux'
 
 const store = configureStore({
-  reducer: rootReducer,
-  devTools: process.env.NODE_ENV !== 'production'
-});
+	reducer: rootReducer,
+	devTools: process.env.NODE_ENV !== 'production',
+})
 
-export type RootState = ReturnType<typeof rootReducer>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof rootReducer>
+export type AppDispatch = typeof store.dispatch
 
-export const useDispatch: () => AppDispatch = () => dispatchHook();
-export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
+export const useDispatch: () => AppDispatch = () => dispatchHook()
+export const useSelector: TypedUseSelectorHook<RootState> = selectorHook
 
-export default store;
+export default store
