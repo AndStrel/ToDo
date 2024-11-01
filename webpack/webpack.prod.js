0a59de540a4e4c5f96cc1a8029069d0ac458242e
Andrey Strelnikov <xoz9ih@outlook.com>
2024-11-01 20:48:00 +0300

    финал

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,9 +2,5 @@ const path = require('path')
 module.exports = {
 	mode: 'production',
 	devtool: false,
-	// output: {
-	// 	path: path.resolve(__dirname, './dist'),
-	// 	filename: 'bundle.js',
-	// 	publicPath: production ? '/ToDo/' : '/', // <-- замените 'ToDo' на название вашего репозитория
-	// },
+
 }
